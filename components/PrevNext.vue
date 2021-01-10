3224da075cb94614979a5b97d94d0dfe6fc5fc3c
zaephyr <uros.mahne@gmail.com>
2021-01-10 17:11:37 +0100

    home icon center on small and lower screens

--- components/PrevNext.vue
+++ components/PrevNext.vue
@@ -3,7 +3,7 @@
         <NuxtLink
             v-if="prev"
             :to="{ name: 'projects-slug', params: { slug: prev.slug } }"
-            class="font-bold hover:underline"
+            class="font-bold hover:underline w-2/5 md:w-auto"
         >
             <fa-icon :icon="['fas', 'angle-double-left']" />
 
@@ -12,14 +12,14 @@
         <span v-else>&nbsp;</span>
         <NuxtLink
             to="/#portfolio"
-            class="text-primary-dark text-2xl md:text-3xl md:border-b-2 border-primary-dark absolute"
+            class="text-primary-dark text-2xl md:text-3xl border-b-2 border-primary-dark md:absolute"
         >
             <fa-icon :icon="['fas', 'home']" />
         </NuxtLink>
         <NuxtLink
             v-if="next"
             :to="{ name: 'projects-slug', params: { slug: next.slug } }"
-            class="font-bold hover:underline"
+            class="font-bold hover:underline text-right w-2/5 md:w-auto"
         >
             <span :style="{ color: next.color }">{{ next.title }}</span>
             <fa-icon :icon="['fas', 'angle-double-right']" />
